75201bbf7d67c98bf6e25a735ee30f1cb3248d69
d-sauer-9 <79286011+d-sauer-9@users.noreply.github.com>
2021-04-13 13:28:41 -0500

    Update genBankRetrieval_16s_update.py

--- genBankRetrieval_16s_update.py
+++ genBankRetrieval_16s_update.py
@@ -1,44 +1,37 @@
 #getting 16s rRNA Sequences
 
 from Bio import SeqIO, Entrez,SeqFeature
-import sys
 
 #take list of species names and search Genback using
 #Entrez to find 16S region
 
+#File IO
+filename = "C:\\Users\\Delaney\\Desktop\\taxa_names.txt"
+file = open(filename, 'r')
+speList = file.read().split("\n")
+#print(speList[0])
 
-#temporary single term
-#eventually, this will run in a loop to take every sepcies name from a text file and find the 16s seq
-Entrez.email = "rrajagopal@luc.edu"
-name = '"E.coli"[Organism]'
-handle = Entrez.esearch(db="nucleotide", term = name) #searching genbak via Entrez
-record = Entrez.read(handle)
-idList = record["IdList"] #Id list of entries that match the term
-#print(idList)
-#use the first id list match to search for specific nucleotide sequences
-speHandle = Entrez.efetch(db="nucleotide",id = idList[1],rettype = "gb", retmode = 'text')
-#speRecord = SeqIO.parse(speHandle,"genbank")
-#print(speRecord)
+Entrez.email = "rrajagopal@luc.edu" #for entrez search in loop
 
+for name in speList:
+    handle = Entrez.esearch(db="nucleotide", term = name) #searching genbak via Entrez
+    record = Entrez.read(handle)
+    idList = record["IdList"] #Id list of entries that match the term
+    #use the first id list match to search for specific nucleotide sequences
+    speHandle = Entrez.efetch(db="nucleotide",id = idList[0],rettype = "gb", retmode = 'text')
+    speRecord = SeqIO.read(speHandle,"genbank")
+    #print(speRecord)
 
-#attempting to piece together the upper and bottom chunks of code
-
-#for genome in speRecord:
-for record in SeqIO.parse(speHandle,'genbank'):
-    for gene in record.features:
-        print(gene)
+    for gene in speRecord.features:
         if gene.type=="rRNA": 
             if 'product' in gene.qualifiers:
                 if '16S' in gene.qualifiers['product'][0]:
                     start = gene.location.nofuzzy_start
                     end = gene.location.nofuzzy_end
                     if 'db_xref' in gene.qualifiers:
-                        gi=[]
-                        gi=str(gene.qualifiers['db_xref'])
-                        gi=gi.split(":")[1]
-                        gi=gi.split("'")[0]
-                        print (">GeneId|%s|16S rRNA|%s\n%s" % (gi,genome.description,genome.seq[start:end]))
-                    else:
-                        print (">GeneId|NoGenID|16S rRNA|%s\n%s" % (genome.description,genome.seq[start:end]))
+                        dbID=str(gene.qualifiers['db_xref'])
+                        print(">" + dbID + " name 16S rRNA sequence" + "\n" + str(speRecord.seq[start:end]))
+                        break
+
 
         
